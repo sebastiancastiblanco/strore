d219bbffc081af6a0d90b065c437730b5984c400
sebastiancastiblanco <sebastiancasti@gmail.com>
2013-12-28 09:18:11 -0500

    primet commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'ecf7de957ffaaa11733e8d8709b38602f0c863e7fe51663f3e874c68ff1cfee23fe226cf32fe9b14d4bb1d5a718aa5211f3bdffc47792136406576dec0f213ec'
